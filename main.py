7f1026753610955c213721f17b4fca1c7601fdcb
saburay32 <saburay32@gmail.com>
2023-02-01 00:11:39 +0300

    full code from 1st lection

--- main.py
+++ main.py
@@ -14,7 +14,15 @@ class StackFrontier():
         self.frontier.append(node)
 
     def contains_state(self, state):
-        return any(node.state == state for in self.frontier)
+        return any(node.state == state for node in self.frontier)
 
     def empty(self):
         return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception('empty frontier')
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[-1]
+            return node
